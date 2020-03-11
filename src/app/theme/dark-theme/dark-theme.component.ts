a7b445660a8777d2d361025bfe28422e995ba3c1
Krzysztof RossiKlimkiewicz <krzysztof.rossiklimkiewicz@betssongroup.com>
2020-03-11 10:20:00 +0100

    re add template to dark theme

--- src/app/theme/dark-theme/dark-theme.component.ts
+++ src/app/theme/dark-theme/dark-theme.component.ts
@@ -3,10 +3,10 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-dark-theme',
+  template: '',
   styleUrls: ['./dark-theme.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-
 export class ThemeComponent implements OnInit {
 
   constructor() { }
